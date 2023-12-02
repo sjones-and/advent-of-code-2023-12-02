6c0e3f2028f971442300b27e374b654bee9c9b26
Stuart Jones <47328516+sjones-and@users.noreply.github.com>
2023-12-02 10:28:55 +0000

    Day two solution

--- part-one.py
+++ part-one.py
@@ -2,11 +2,30 @@
 
 import os
 
+max_values = {
+    'red': 12,
+    'green': 13,
+    'blue': 14
+}
+
+def get_game_value(data):
+    parts = data.split(':')
+    game_number = int(parts[0][5:])
+    hands = parts[1].split(';')
+    for hand in hands:
+        colors = hand.strip().split(',')
+        colors = [color.strip().split(' ') for color in colors]
+        colors = {color[1].strip(): int(color[0].strip()) for color in colors }
+        for color, count in colors.items():
+            if count > max_values.get(color, 0):
+                return 0
+    return game_number
+
 def answer(input_file):
     with open(input_file, "r") as input:
         data = input.read().split('\n')
 
-    answer = None
+    answer = sum([get_game_value(game) for game in data])
 
     print(f'The answer is: {answer}')
 
--- part-two.py
+++ part-two.py
@@ -1,12 +1,31 @@
 #!/usr/bin/env python3
 
 import os
+from functools import reduce
+
+def get_game_power(data):
+    parts = data.split(':')
+    game_number = int(parts[0][5:])
+    hands = parts[1].split(';')
+    min_colors = {
+        'red': 0,
+        'green': 0,
+        'blue': 0
+    }
+    for hand in hands:
+        colors = hand.strip().split(',')
+        colors = [color.strip().split(' ') for color in colors]
+        colors = {color[1].strip(): int(color[0].strip()) for color in colors }
+        for color, count in colors.items():
+            if count > min_colors.get(color, 0):
+                min_colors[color] = count
+    return reduce(lambda x, y: x*y, min_colors.values())
 
 def answer(input_file):
     with open(input_file, "r") as input:
         data = input.read().split('\n')
 
-    answer = None
+    answer = sum([get_game_power(game) for game in data])
 
     print(f'The answer is: {answer}')
 
